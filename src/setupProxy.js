ceca0f2a321e5065900c616a83c89233d76817c3
Emanuel Sanchez <esanchezc.sqa@gmail.com>
2024-05-28 13:41:01 -0600

    Update setupProxy to work on Docker

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  const target = process.env.REACT_APP_NODE_APP_URL || 'http://localhost:3001';
   app.use(
     ['/words', '/languages'],
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target,
       changeOrigin: true,
     })
   );
