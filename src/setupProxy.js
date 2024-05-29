6a8f3206ca04a4fdd40430e31dd3f3fbf340fd21
Emanuel Sanchez <esanchezc.sqa@gmail.com>
2024-05-29 11:43:40 -0600

    Update target

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  const target = process.env.REACT_APP_NODE_APP_URL || 'http://localhost:3001';
+module.exports = function (app) {
   app.use(
     ['/words', '/languages'],
     createProxyMiddleware({
-      target,
+      target: 'http://node-app:3001',
       changeOrigin: true,
     })
   );
