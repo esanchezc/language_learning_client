337da644dd12df914cb2f624ed18764a595697dc
Emanuel Sanchez <esanchezc.sqa@gmail.com>
2024-08-04 18:58:15 -0500

    Update cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const webpackPreprocessor = require("@cypress/webpack-preprocessor");
 
 module.exports = defineConfig({
   component: {
@@ -10,5 +11,30 @@ module.exports = defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      const options = {
+        webpackOptions: {
+          resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+          },
+          module: {
+            rules: [
+              {
+                test: /\.tsx?$/,
+                use: [
+                  {
+                    loader: "ts-loader",
+                    options: { transpileOnly: true }
+                  }
+                ],
+                exclude: /node_modules/
+              }
+            ]
+          }
+        },
+      };
+      on("file:preprocessor", webpackPreprocessor(options));
+    },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
 });
